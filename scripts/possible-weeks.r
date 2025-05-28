59e26322589e8efddd168b244f6edd5888151700
Jo Dudding <j.m.dudding@gmail.com>
2025-05-28 20:59:31 +1200

    try site build on github again

---
+++ scripts/possible-weeks.r
@@ -0,0 +1,85 @@
+#-------------------------------------------------------------------------------
+#' possible-weeks.r
+#-------------------------------------------------------------------------------
+#' jo dudding
+#' May 2025
+#' explore the different weeks
+#-------------------------------------------------------------------------------
+
+source("scripts/_setup.r")  
+  
+#--- download the tt week information ---
+
+tt_week <- tt_available()
+
+#--- Base R Penguins ---
+
+#peng <- tt_load("2025-04-15")
+#walk(peng, glimpse)
+
+#--- Seismic Events at Mount Vesuvius ---
+
+vesuvius <- tt_load("2025-05-13")
+walk(vesuvius, glimpse)
+
+#--- Great British Bakeoff ---
+
+bakeoff <- tt_load("2022-10-25")
+walk(bakeoff, glimpse)
+
+#--- Bechdel Test ---
+
+bechdel <- tt_load("2021-03-09")
+walk(bechdel, glimpse)
+
+#--- Australian Fires ---
+
+fire <- tt_load("2020-01-07")
+walk(fire, glimpse)
+
+#--- Marble Races ---
+
+marbles <- tt_load("2020-06-02")
+walk(marbles, glimpse)
+
+#--- US Wind Farm locations ---
+
+wind <- tt_load("2018-11-06")
+walk(wind, glimpse)
+
+#--- roy kent ---
+
+roy <- tt_load("2023-09-26")
+walk(roy, glimpse)
+
+#--- ask a manager salary survey ---
+
+ask_a_manager <- tt_load("2021-05-18")
+walk(ask_a_manager, glimpse)
+
+#--- David Robinson's TidyTuesday Functions ---
+drob <- tt_load("2024-07-09")
+walk(drob, glimpse)
+
+#--- Art Collections ---
+
+tate <- tt_load("2021-01-12")
+walk(tate, glimpse)
+
+#--- lego ---
+
+lego <- tt_load("2022-09-06") 
+walk(lego, glimpse)
+
+#--- Solar/Wind utilities ---
+
+solar_wind <- tt_load("2022-05-03")
+walk(solar_wind, glimpse)
+
+#--- Stack Overflow Annual Developer Survey 2024 ---
+stack <- tt_load("2024-09-03")
+walk(stack, glimpse)
+
+
+
+#-------------------------------------------------------------------------------
